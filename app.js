7bcc980462741d66ab4f2b38293a1d5decd2c46c
Michaela Wagner <mickywagner@gmail.com>
2020-04-17 22:58:37 -0600

    Remove comment from app, add node version to package.json

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// app.post('/new', function(req, res, next) {
-//   console.log(req.body.user)
-// })
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

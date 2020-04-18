fe09456cb7cb512249f0bb14c93798260b9668d3
Michaela Wagner <mickywagner@gmail.com>
2020-04-17 20:35:50 -0600

    Create message array and display in index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var messages = [
+  {text: "Tell me about your quarantine projects", user: "Micky", added: new Date()},
+  {text: "I'm learning Node and Express!", user: "Eruvande", added: new Date()}
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to the mini-message board' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 router.get('/new', function(req, res, next) {

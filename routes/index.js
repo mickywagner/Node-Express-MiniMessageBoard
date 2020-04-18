90df8a4a9b1e783c9e59df09fe470c79e70a0c13
Michaela Wagner <mickywagner@gmail.com>
2020-04-17 22:38:06 -0600

    Add post method to push form content to message array

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 var messages = [
   {text: "Tell me about your quarantine projects", user: "Micky", added: new Date()},
   {text: "I'm learning Node and Express!", user: "Eruvande", added: new Date()}
@@ -14,5 +16,16 @@ router.get('/', function(req, res, next) {
 router.get('/new', function(req, res, next) {
   res.render('new', { title: 'Enter new message'})
 })
+
+router.post('/new', function(req, res, next) {
+   const message = req.body.messageText
+   const user = req.body.user
+
+   messages.push({text: message, user: user, added: new Date()})
+  
+   console.log(messages)
+})
+
+
 
 module.exports = router;

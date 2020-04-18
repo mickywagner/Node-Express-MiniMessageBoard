1b6cd50361ea0eef17bd961f41363fa1888afe07
Michaela Wagner <mickywagner@gmail.com>
2020-04-17 20:21:16 -0600

    Add '/new' route and newmessage file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to the mini-message board' });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Enter new message'})
+})
 
 module.exports = router;
